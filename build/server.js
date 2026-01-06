8b19badee3142d0de512e34e3e9b8fce751f4c9a
jammehabdou64 <jammehabdou64@gmail.com>
2026-01-06 13:05:09 +0000

    add log

--- build/server.js
+++ build/server.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./bootstrap/app");
+const jcc_express_mvc_1 = require("jcc-express-mvc");
+console.log({ config: jcc_express_mvc_1.config.get("APP_ENV") });
 app_1.app.run();
--- server.ts
+++ server.ts
@@ -1,3 +1,6 @@
 import { app } from "./bootstrap/app";
+import { config } from "jcc-express-mvc";
+
+console.log({ config: config.get("APP_ENV") });
 
 app.run();
